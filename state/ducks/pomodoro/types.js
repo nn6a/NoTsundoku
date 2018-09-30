bb94c6ca1d5a6820fc082dde5cf99b6b9470d64c
nn6a <869tjmw@gmail.com>
2018-09-30 15:09:01 +0900

    refactor: small tweaks in action types

--- state/ducks/pomodoro/types.js
+++ state/ducks/pomodoro/types.js
@@ -1,6 +1,6 @@
-const START_TIMER = 'START_TIMER';
-const RESTART_TIMER = 'RESTART_TIMER';
-const ADD_SECOND = 'ADD_SECOND';
+const START_TIMER = 'app/pomodoro/START_TIMER';
+const RESTART_TIMER = 'app/pomodoro/RESTART_TIMER';
+const ADD_SECOND = 'app/pomodoro/ADD_SECOND';
 
 export {
     START_TIMER,
