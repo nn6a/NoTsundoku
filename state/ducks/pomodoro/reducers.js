882040222f1cd969c2ea2f87334430b6858282fe
nn6a <869tjmw@gmail.com>
2018-10-04 18:30:31 +0900

    remove unused lines

--- state/ducks/pomodoro/reducers.js
+++ state/ducks/pomodoro/reducers.js
@@ -11,44 +11,6 @@ const initialState = {
     pomodoroCount: 0
 };
 
-// function startPomodoro (state) {
-//     return {
-//         ...state,
-//         isPomodoro: true
-//     };
-// }
-
-
-// function applyRestartTimer (state) {
-//     return {
-//         ...state,
-//         isPomodoro: false,
-//         isBreak: false,
-//         elapsedTime: 0,
-//         timerDuration: CONSTANTS.POMODORO_DURATION
-//     };
-// }
-
-// function applyAddSecond (state) {
-//     const currentDuration = state.isBreak ? CONSTANTS.BREAK_DURATION : CONSTANTS.POMODORO_DURATION;
-//     const nextDuration = !state.isBreak ? CONSTANTS.BREAK_DURATION : CONSTANTS.POMODORO_DURATION;
-//
-//     if (state.elapsedTime < currentDuration) {
-//         return {
-//             ...state,
-//             elapsedTime: state.elapsedTime + 1
-//         };
-//     } else {
-//         return {
-//             ...state,
-//             isBreak: !state.isBreak,
-//             timerDuration: nextDuration,
-//             elapsedTime: 0,
-//             pomodoroCount: state.isBreak ? state.pomodoroCount : (state.pomodoroCount + 1)
-//         };
-//     }
-// }
-
 function pomodoroReducer (state = initialState, action) {
     switch (action.type) {
         case START_POMODORO:
